9c718b47af90b30c72335a9e24f5a081c3e7fb3c
mc-santiago <mari.c.santiago@europe.com>
2019-07-18 11:55:51 +0200

    basic scroll working at home

--- src/Components/Scroll.js
+++ src/Components/Scroll.js
@@ -3,8 +3,13 @@ import React, { Component } from 'react'
 export default class Scroll extends Component {
     render() {
         return (
-            <div>
-                
+            <div className="scroll-horiz">
+                <div className="scroll-box"></div>
+                <div className="scroll-box"></div>
+                <div className="scroll-box"></div>
+                <div className="scroll-box"></div>
+                <div className="scroll-box"></div>
+                <div className="scroll-box"></div>
             </div>
         )
     }
