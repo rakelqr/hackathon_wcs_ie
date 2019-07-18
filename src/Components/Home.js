83528ae80c1e1d45cbeb42aa51140f6ee6994d7d
mc-santiago <mari.c.santiago@europe.com>
2019-07-18 16:17:57 +0200

    home page basic look

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,15 +1,19 @@
-import React, { Component } from 'react'
-import Scroll from './Scroll'
+import React, { Component } from 'react';
+import NewLogoSquare from './Images/NewLogoSquare.png';
+import Scroll from './Scroll';
 
 export default class Home extends Component {
     render() {
         return (
             <React.Fragment>
-                <header>Recomentados</header>
+                <div className='logoTop'>
+                    <img src={NewLogoSquare} alt='logo menu-finder red square' height='100'/>
+                </div>
+                <header className='headerHome'>Recomendados</header>
                     <Scroll/>
-                <header>Novedades</header>
+                <header className='headerHome'>Novedades</header>
                     <Scroll/>
-                <header>Populares</header>
+                <header className='headerHome'>Populares</header>
                     <Scroll/>
             </React.Fragment>
         )
