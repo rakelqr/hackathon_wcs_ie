0398cf9b40000806568dee067a6b07f81d7f144f
mc-santiago <mari.c.santiago@europe.com>
2019-07-18 11:03:23 +0200

    basic react router installed

---
+++ src/Components/Scroll.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Scroll extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
